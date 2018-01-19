acd916fb74ce193f0519d68284422e20966f962e
Aydar Fayzullin <aydar_fayzullin@yahoo.com>
2018-01-18 20:16:21 -0500

    Created Title component & updated TextBoxes component

--- index.vr.js
+++ index.vr.js
@@ -7,15 +7,35 @@ import {
   View,
 } from 'react-vr';
 
+class Title extends React.Component {
+  constructor() {
+    super();
+    this.state = {title: 'Panoramic Parks'};
+  }
+  render() {
+    return (
+      <View>
+        <Text style={{
+          fontSize: 0.2,
+          textAlign: 'center',
+          color: "#457"
+        }}>
+          {this.state.title}
+        </Text>
+      </View>
+    )
+  }
+} 
+
 class TextBoxes extends React.Component {
   render () {
     return (
         <View>
-          <View style={{margin: 0.1, height: 0.3, backgroundColor: '#457'}}><Text style={{fontSize: 0.2, textAlign: 'center'}}>Grand Canyon</Text></View>
-          <View style={{margin: 0.1, height: 0.3, backgroundColor: '#457'}}><Text style={{fontSize: 0.2, textAlign: 'center'}}>Yosemite</Text></View>
-          <View style={{margin: 0.1, height: 0.3, backgroundColor: '#457'}}><Text style={{fontSize: 0.2, textAlign: 'center'}}>Bryce</Text></View>
-          <View style={{margin: 0.1, height: 0.3, backgroundColor: '#457'}}><Text style={{fontSize: 0.2, textAlign: 'center'}}>Yellowstown</Text></View>
-          <View style={{margin: 0.1, height: 0.3, backgroundColor: '#457'}}><Text style={{fontSize: 0.2, textAlign: 'center'}}>Acadia</Text></View>  
+          <View style={{margin: 0.1, height: 0.3, backgroundColor: '#457'}}><Text style={{fontSize: 0.2, textAlign: 'center'}}>{this.props.parks.GrandCanyon}</Text></View>
+          <View style={{margin: 0.1, height: 0.3, backgroundColor: '#457'}}><Text style={{fontSize: 0.2, textAlign: 'center'}}>{this.props.parks.Yosemite}</Text></View>
+          <View style={{margin: 0.1, height: 0.3, backgroundColor: '#457'}}><Text style={{fontSize: 0.2, textAlign: 'center'}}>{this.props.parks.Bryce}</Text></View>
+          <View style={{margin: 0.1, height: 0.3, backgroundColor: '#457'}}><Text style={{fontSize: 0.2, textAlign: 'center'}}>{this.props.parks.Yellowstown}</Text></View>
+          <View style={{margin: 0.1, height: 0.3, backgroundColor: '#457'}}><Text style={{fontSize: 0.2, textAlign: 'center'}}>{this.props.parks.Acadia}</Text></View>  
         </View>
       )
   }
@@ -23,6 +43,14 @@ class TextBoxes extends React.Component {
 
 export default class PanoramicParks extends React.Component {
   render() {
+    const parks = {
+      GrandCanyon: 'Grand Canyon',
+      Yosemite: 'Yosemite',
+      Bryce: 'Bryce',
+      Yellowstown: 'Yellowstown',
+      Acadia: 'Acadia'
+    }
+
     return (
       <View>
         <Pano source={asset('horseshoebend.jpg')}/>
@@ -35,7 +63,8 @@ export default class PanoramicParks extends React.Component {
                 transform: [{translate: [0,0,-5]}],
                 layoutOrigin: [0.5,0.5]			
     		  }}>
-    		  <TextBoxes />  
+          <Title />
+    		  <TextBoxes parks ={parks}/>  
     		</View>     
       </View>
     );
